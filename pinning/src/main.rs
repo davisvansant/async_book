c50326f248a13d3011944b92f64af9143db31556
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-04-15 11:19:23 -0700

    pinning

--- pinning/src/main.rs
+++ pinning/src/main.rs
@@ -1,3 +1,26 @@
+// use pin_utils::pin_mut;
+use futures::Future;
+use futures::pin_mut;
+
+fn execute_unpin_future(x: impl Future<Output = ()> + Unpin) {}
+
+
+
 fn main() {
     println!("Hello, world!");
+
+    let fut = async {
+
+    };
+
+    // execute_unpin_future(fut);
+
+    let futurarma = async {};
+    let futfut = Box::pin(futurarma);
+    execute_unpin_future(futfut);
+    let futfutfut = async {
+
+    };
+    pin_mut!(futfutfut);
+    execute_unpin_future(futfutfut);
 }
