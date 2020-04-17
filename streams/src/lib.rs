dc9c2b9073cc7496e3046d859b63646401a21333
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-04-16 19:03:27 -0700

    updated streams library

--- streams/src/lib.rs
+++ streams/src/lib.rs
@@ -4,12 +4,14 @@ use futures::task::Context;
 use std::pin::Pin;
 use futures::channel::mpsc;
 use futures::prelude::*;
+use futures::executor::block_on;
+use std::io;
 
-trait Stream {
-    type Item;
-
-    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>>;
-}
+// trait Stream {
+//     type Item;
+//
+//     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>>;
+// }
 
 async fn send_recv() {
     const BUFFER_SIZE: usize = 10;
@@ -24,3 +26,45 @@ async fn send_recv() {
     assert_eq!(Some(2), rx.next().await);
     assert_eq!(None, rx.next().await);
 }
+
+async fn sum_with_next(mut stream: Pin<&mut dyn Stream<Item = i32>>) -> i32 {
+    use futures::stream::StreamExt;
+
+    let mut sum = 0;
+    while let Some(item) = stream.next().await {
+        sum += item;
+    }
+
+    sum
+}
+
+async fn sum_with_try_next(
+    mut stream: Pin<&mut dyn Stream<Item = Result<i32, io::Error>>>) -> Result<i32, io::Error> {
+    use futures::stream::TryStreamExt;
+
+    let mut sum = 0;
+    while let Some(item) = stream.try_next().await? {
+        sum += item;
+    }
+    
+    Ok(sum)
+}
+
+async fn jump_around(
+    mut stream: Pin<&mut dyn Stream<Item = Result<u8, io::Error>>>,
+) -> Result<(), io::Error> {
+    use futures::stream::TryStreamExt;
+
+    const MAX_CONCURRENT_JUMPERS: usize = 100;
+
+    stream.try_for_each_concurrent(MAX_CONCURRENT_JUMPERS, |num| async move {
+        jump_n_times(num).await?;
+        report_n_jumps(num).await?;
+        Ok(())
+    }).await?;
+
+    Ok(())
+}
+
+async fn jump_n_times(_: u8) -> Result<(), io::Error> { Ok(()) }
+async fn report_n_jumps(_: u8) -> Result<(), io::Error> { Ok(()) }
