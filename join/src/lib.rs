e0587eb5a8370874bd7d886445465a26c04d8c6c
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-04-17 10:45:05 -0700

    join

--- join/src/lib.rs
+++ join/src/lib.rs
@@ -1,3 +1,18 @@
+use futures::join;
+
+struct Book;
+struct Music;
+
+async fn get_book() -> Book { Book }
+async fn get_music() -> Music { Music }
+
+
+async fn get_book_and_music() -> (Book, Music) {
+    let book_fut = get_book();
+    let music_fut = get_music();
+    join!(book_fut, music_fut)
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
