1546a5d34c9fab6efae40034078821acfa5fde37
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-04-20 12:24:32 -0700

    select loop with fuse

--- select/src/lib.rs
+++ select/src/lib.rs
@@ -1,5 +1,5 @@
 use futures::{
-    future::FutureExt,
+    future::{Fuse, FusedFuture, FutureExt},
     pin_mut,
     select,
     future,
@@ -55,6 +55,30 @@ async fn add_two_streams(
         }
     }
     total
+}
+
+async fn get_new_num() -> u8 { 5 }
+async fn run_on_new_num(_: u8) {}
+
+async fn run_loop(
+    mut interval_timer: impl Stream<Item = ()> + FusedStream + Unpin,
+    starting_num: u8,
+) {
+    let run_on_new_num_fut = run_on_new_num(starting_num).fuse();
+    let get_new_num_fut = Fuse::terminated();
+    pin_mut!(run_on_new_num_fut, get_new_num_fut);
+
+    loop {
+        select! {
+            () = interval_timer.select_next_some() => {
+                if get_new_num_fut.is_terminated() {
+                    get_new_num_fut.set(get_new_num().fuse());
+                }
+            },
+            () = run_on_new_num_fut => {},
+            complete => panic!("`interval_timer` completed unexepectedly"),
+        }
+    }
 }
 
 #[cfg(test)]
